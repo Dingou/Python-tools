816c0d6d48fc9dc20ae9b0d3d4d13d061901dd1c
Dingou <88981382@qq.com>
2016-11-21 22:12:20 +0800

    update searchlog2： final edition

--- System_func/log_search/searchlog2.py
+++ System_func/log_search/searchlog2.py
@@ -10,7 +10,7 @@ import socket
 def gzip_file_process(logfile=''):
     try:
         execute('gzip -d %s' % logfile)
-        ungzip_file = re.sub("\.gzip$", "", logfile)
+        ungzip_file = re.sub("\.gz$", "", logfile)
         log_grep(ungzip_file)
         execute('gzip %s' % ungzip_file)
     except Exception as e:
@@ -43,6 +43,11 @@ def tar_gzip_file_process(logfile=''):
     except Exception as e:
         print "failed grep and filter .tar.gzip log with keyworks due to %s" % e
 
+def normal_file_process(logfile=''):
+    try:
+        log_grep(logfile)
+    except Exception as e:
+        print "failed grep and filter .tar.gzip log with keyworks due to %s" % e
 
 log_keyword_grep = 'grep "succeeded in" %s |awk -F\'s:\' \'{print $1}\''
 log_remove_dupli = 'awk \'{ print $2}\' worker1.c1.ylly.co-celery-purify.log|awk -F\'[\' \'{ print $1}\''
@@ -53,8 +58,9 @@ pattern_end_zip = re.compile(r"\.zip$")
 pattern_end_tar_gz = re.compile(r"\.tar\.gz$")
 
 pattern_list = [pattern_end_gz, pattern_end_tar, pattern_end_zip, pattern_end_tar_gz]
-pattern_dict = {'pattern_end_gz': gzip_file_process, 'pattern_end_tar': tar_file_process,
-                'pattern_end_zip': zip_file_process, 'pattern_end_tar_gz': tar_gzip_file_process}
+pattern_dict = {pattern_end_gz: gzip_file_process, pattern_end_tar: tar_file_process,
+                pattern_end_zip: zip_file_process, pattern_end_tar_gz: tar_gzip_file_process,
+                'pattern_no_compress': normal_file_process}
 
 
 pattern_celery_log = re.compile(r"^celery.log(-\d+\.gz)?$")
@@ -89,6 +95,7 @@ def choose_pattern(logfile):
         else:
             i += 1
             continue
+    return 'pattern_no_compress'
 
 def filter_output(output='',comp_condition='= 0'):
     purifylog = "%s-%s-purify.log" % (hostname,service)
@@ -110,7 +117,7 @@ def log_filter(logfile=''):
     else:
         try:
             pattern = choose_pattern(logfile)
-            pattern_dict['pattern'](logfile)
+            pattern_dict[pattern](logfile)
         except Exception as e:
             print "failed merging log due to %s" % e
         else:
